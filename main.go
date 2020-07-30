60adef9864ecc3fe71ed526bcf85166475c594e4
ckeivin <chengkeivin91@gmail.com>
2020-07-30 14:26:27 +0800

    working dryRun

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
@@ -21,40 +22,116 @@ var (
 
 // HandleRequest handles lambda requests
 func HandleRequest(ctx context.Context) (map[string]map[string]string, error) {
-	fmt.Printf("Configured region is [%v]\n", awsRegion)
 
 	// Create Lambda service client
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
-	// client := lambda.New(sess, &aws.Config{Region: aws.String(awsRegion)})
 
 	// Create new EC2 client
 	svc := ec2.New(sess)
-	// get tags
-	tagKey, tagValues := getTags()
+	// get environment vars
+	fmt.Println("## Environment Variables ##")
+	fmt.Printf("'AWS_REGION' is set to [%v]\n", awsRegion)
+	tagKey, tagValues, dryRun := getTags()
 	amiAge := getAmiAge()
+
 	// format inputs
+	// debug
+	fmt.Println("## AMI Filters ##")
+	fmt.Printf("tagKey: [%v]\n", tagKey)
 	input := formatInput(tagKey, tagValues, amiAge)
 	ami, err := svc.DescribeImages(input)
 	if err != nil {
 		fmt.Println("there was an error listing instances in", err.Error())
 		log.Fatal(err.Error())
 	}
+	// get AMI creation time
+	creationDatesMap := getAmiAgeMap(ami)
 
 	// get AMI to snapshots mapping
-	snapshotsMap := getSnapshots(ami)
-	fmt.Printf("%+v", snapshotsMap)
-	return snapshotsMap, nil
+	snapshotsMap := getSnapshotMap(ami)
+
+	// get finalSnapshots
+	finalSnapshotMap := getFinalSnapshotMap(amiAge, creationDatesMap, snapshotsMap)
+
+	// summary
+	fmt.Println("## Summary ##")
+	fmt.Printf("Total AMIs: %v\n", len(finalSnapshotMap))
+	totalSnapshots := 0
+	for _, v1 := range finalSnapshotMap {
+		for range v1 {
+			totalSnapshots++
+		}
+	}
+	fmt.Printf("Total Snapshots: %v\n", totalSnapshots)
+
+	// deregister image
+	fmt.Println("## Running Deregister Jobs ##")
+	deregisterAMI(svc, finalSnapshotMap, dryRun)
+	return finalSnapshotMap, nil
 }
 func main() {
 	lambda.Start(HandleRequest)
 
 }
 
-// getSnapshots gets all AMI Ids and respective snapshot Ids
-func getSnapshots(ami *ec2.DescribeImagesOutput) (snapshotsMap map[string]map[string]string) {
+func deregisterAMI(svc *ec2.EC2, getFinalSnapshotMap map[string]map[string]string, dryRun bool) {
+
+	for i := range getFinalSnapshotMap {
+		// create inputs
+		imageInput := &ec2.DeregisterImageInput{
+			DryRun:  &dryRun,
+			ImageId: aws.String(i),
+		}
+		// fmt.Println(imageInput)
+		// deregister
+		_, err := svc.DeregisterImage(imageInput)
+		if err != nil {
+			fmt.Printf("[%v] - '%v'\n", *imageInput.ImageId, err.Error())
+			if strings.Contains(err.Error(), "DryRunOperation") {
+				continue
+			} else {
+				log.Fatal(err.Error())
+			}
+		}
+		// println()
+		// fmt.Println(deregister)
+	}
+
+	return
+}
+func getFinalSnapshotMap(amiAge int, creationMap map[string]int, snapshotsMap map[string]map[string]string) (finalSnapshotMap map[string]map[string]string) {
+	finalSnapshotMap = snapshotsMap
+	for i, v := range creationMap {
+		if v < amiAge {
+			delete(finalSnapshotMap, i)
+		}
+	}
+	return
+}
+func getAmiAgeMap(ami *ec2.DescribeImagesOutput) (creationMap map[string]int) {
+
+	creationMap = map[string]int{}
+	getTime := time.Now()
+
+	for _, v1 := range ami.Images {
+
+		creationTime, err := time.Parse(time.RFC3339, *v1.CreationDate)
+		if err != nil {
+			panic(err)
+		}
+		daysDiff := int(getTime.Sub(creationTime).Hours() / 24)
+		creationMap[*v1.ImageId] = daysDiff
+	}
+
+	return
+}
+
+// getSnapshotMap gets all AMI Ids and respective snapshot Ids
+func getSnapshotMap(ami *ec2.DescribeImagesOutput) (snapshotsMap map[string]map[string]string) {
 	snapshotsMap = map[string]map[string]string{}
+
 	for _, v1 := range ami.Images {
 		snapshotsMap[*v1.ImageId] = map[string]string{}
 		for _, v2 := range v1.BlockDeviceMappings {
@@ -63,6 +140,7 @@ func getSnapshots(ami *ec2.DescribeImagesOutput) (snapshotsMap map[string]map[st
 	}
 	return
 }
+
 func getAmiAge() (amiAge int) {
 
 	amiAge, err := strconv.Atoi(os.Getenv("AMI_AGE"))
@@ -76,25 +154,28 @@ func getAmiAge() (amiAge int) {
 	println()
 	return
 }
-
-func getTags() (tagKey, tagValues string) {
+func getTags() (tagKey, tagValues string, dryRun bool) {
 
 	tagKey = os.Getenv("TAG_KEY")
 	if len(tagKey) == 0 {
-		fmt.Println()
 		fmt.Println("'TAG_KEY' is not set. Default value of 'name' is used")
 		tagKey = "name"
 	} else {
 		fmt.Printf("'TAG_KEY' of [%v] is set\n", tagKey)
 	}
-
 	tagValues = os.Getenv("TAG_VALUES")
 	if len(tagValues) == 0 {
-		fmt.Println()
 		fmt.Println("'TAG_VALUES' is not set. Default value of 'windows2016-base' is used")
 		tagValues = "windows2016-base"
 	} else {
 		fmt.Printf("'TAG_VALUES' of [%v] is set\n", tagValues)
+	}
+
+	dryRun, err := strconv.ParseBool(os.Getenv("DRY_RUN"))
+	if err != nil {
+		log.Fatal("'DRY_RUN' value is invalid. Valid entries are 'true' or 'false'\n", err.Error())
+	} else {
+		fmt.Printf("'DRY_RUN' of [%v] is set\n", dryRun)
 	}
 
 	return
@@ -108,9 +189,6 @@ func formatInput(tagKey, tagValues string, amiAge int) (input *ec2.DescribeImage
 	for _, v := range strings.Split(tagValues, ";") {
 		tagValueSlice = append(tagValueSlice, strings.TrimSpace(v)+"*")
 	}
-	// debug
-	fmt.Println("Filter options:")
-	fmt.Printf("tagKey: [%v]\n", tagKey)
 	for i, v := range tagValueSlice {
 		fmt.Printf("tagValues[%d]:[%v]\n", i, v)
 	}
